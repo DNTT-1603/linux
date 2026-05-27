d33286609f21bae2ec095ec88b768d9f9f225f17
tudo <dongocthanhtuwork@gmail.com>
2026-05-27 11:33:57 +0900

    Migrate: st7789v & ili9486 at 62f6ce4 commit

--- drivers/gpu/drm/tiny/ili9486.c
+++ drivers/gpu/drm/tiny/ili9486.c
@@ -14,12 +14,11 @@
 
 #include <video/mipi_display.h>
 
-#include <drm/clients/drm_client_setup.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_drv.h>
-#include <drm/drm_fbdev_dma.h>
+#include <drm/drm_fb_helper.h>
 #include <drm/drm_gem_atomic_helper.h>
-#include <drm/drm_gem_dma_helper.h>
+#include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_managed.h>
 #include <drm/drm_mipi_dbi.h>
 #include <drm/drm_modeset_helper.h>
@@ -43,54 +42,114 @@
 static int waveshare_command(struct mipi_dbi *mipi, u8 *cmd, u8 *par,
 			     size_t num)
 {
-	struct spi_device *spi = mipi->spi;
-	unsigned int bpw = 8;
-	void *data = par;
-	u32 speed_hz;
-	int i, ret;
-	__be16 *buf;
+	if (*cmd == 0x2C) {
+		struct spi_device *spi = mipi->spi;
+		void *data = par;
+		u32 speed_hz;
+		int i, ret;
+		u8 *buf;
 
-	buf = kmalloc(32 * sizeof(u16), GFP_KERNEL);
-	if (!buf)
-		return -ENOMEM;
 
-	/*
-	 * The displays are Raspberry Pi HATs and connected to the 8-bit only
-	 * SPI controller, so 16-bit command and parameters need byte swapping
-	 * before being transferred as 8-bit on the big endian SPI bus.
-	 */
-	buf[0] = cpu_to_be16(*cmd);
-	spi_bus_lock(spi->controller);
-	gpiod_set_value_cansleep(mipi->dc, 0);
-	speed_hz = mipi_dbi_spi_cmd_max_speed(spi, 2);
-	ret = mipi_dbi_spi_transfer(spi, speed_hz, 8, buf, 2);
-	spi_bus_unlock(spi->controller);
-	if (ret || !num)
-		goto free;
+		/*
+		* The displays are Raspberry Pi HATs and connected to the 8-bit only
+		* SPI controller, so 16-bit command and parameters need byte swapping
+		* before being transferred as 8-bit on the big endian SPI bus.
+		* Pixel data bytes have already been swapped before this function is
+		* called.
+		*/
+		// buf[0] = cpu_to_be16(*cmd);
+		gpiod_set_value_cansleep(mipi->dc, 0);
+		speed_hz = mipi_dbi_spi_cmd_max_speed(spi, 1);
+		ret = mipi_dbi_spi_transfer(spi, speed_hz, 8, cmd, 1);
 
-	/* 8-bit configuration data, not 16-bit pixel data */
-	if (num <= 32) {
-		for (i = 0; i < num; i++)
-			buf[i] = cpu_to_be16(par[i]);
-		num *= 2;
-		data = buf;
+
+// 	/* 8-bit configuration data, not 16-bit pixel data */
+			int buffSize = (num / 2) + (num & 1);
+			
+			buf = kmalloc(3 * buffSize * sizeof(u8), GFP_KERNEL);
+			if (!buf)
+				return -ENOMEM;
+
+
+			for (i = 0; i < buffSize; i++) {
+				u16 color = (par[2 * i] << 8) | (par[2 * i + 1]);
+
+				buf[3 * i] = ~((color & 0xF800) >> 8);
+				buf[3 * i + 1] = ~((color & 0x07E0) >> 3);
+				buf[3 * i + 2] = ~((color & 0x1F) << 3);
+
+			}
+
+			num = buffSize * 3;
+			speed_hz = mipi_dbi_spi_cmd_max_speed(spi, num);
+			data = buf;
+
+		gpiod_set_value_cansleep(mipi->dc, 1);
+		ret = mipi_dbi_spi_transfer(spi, speed_hz, 8, data, num);
+		free:
+		kfree(buf);
+
+		return ret;
+	} else {
+		
+		struct spi_device *spi = mipi->spi;
+		unsigned int bpw = 8;
+		u32 speed_hz;
+		int ret;
+
+		gpiod_set_value_cansleep(mipi->dc, 0);
+		speed_hz = mipi_dbi_spi_cmd_max_speed(spi, 1);
+		ret = mipi_dbi_spi_transfer(spi, speed_hz, 8, cmd, 1);
+		if (ret || !num)
+			return ret;
+
+		if (*cmd == MIPI_DCS_WRITE_MEMORY_START && !mipi->swap_bytes)
+			bpw = 16;
+
+		gpiod_set_value_cansleep(mipi->dc, 1);
+		speed_hz = mipi_dbi_spi_cmd_max_speed(spi, num);
+
+		return mipi_dbi_spi_transfer(spi, speed_hz, bpw, par, num);
 	}
+// 	struct spi_device *spi = mipi->spi;
+// 	void *data = par;
+// 	u32 speed_hz;
+// 	int i, ret;
+// 	__be16 *buf;
 
-	/*
-	 * Check whether pixel data bytes needs to be swapped or not
-	 */
-	if (*cmd == MIPI_DCS_WRITE_MEMORY_START && !mipi->swap_bytes)
-		bpw = 16;
+// 	buf = kmalloc(32 * sizeof(u16), GFP_KERNEL);
+// 	if (!buf)
+// 		return -ENOMEM;
 
-	spi_bus_lock(spi->controller);
-	gpiod_set_value_cansleep(mipi->dc, 1);
-	speed_hz = mipi_dbi_spi_cmd_max_speed(spi, num);
-	ret = mipi_dbi_spi_transfer(spi, speed_hz, bpw, data, num);
-	spi_bus_unlock(spi->controller);
- free:
-	kfree(buf);
+// 	/*
+// 	 * The displays are Raspberry Pi HATs and connected to the 8-bit only
+// 	 * SPI controller, so 16-bit command and parameters need byte swapping
+// 	 * before being transferred as 8-bit on the big endian SPI bus.
+// 	 * Pixel data bytes have already been swapped before this function is
+// 	 * called.
+// 	 */
+// 	buf[0] = cpu_to_be16(*cmd);
+// 	gpiod_set_value_cansleep(mipi->dc, 0);
+// 	speed_hz = mipi_dbi_spi_cmd_max_speed(spi, 2);
+// 	ret = mipi_dbi_spi_transfer(spi, speed_hz, 8, buf, 2);
+// 	if (ret || !num)
+// 		goto free;
 
-	return ret;
+// 	/* 8-bit configuration data, not 16-bit pixel data */
+// 	if (num <= 32) {
+// 		for (i = 0; i < num; i++)
+// 			buf[i] = cpu_to_be16(par[i]);
+// 		num *= 2;
+// 		speed_hz = mipi_dbi_spi_cmd_max_speed(spi, num);
+// 		data = buf;
+// 	}
+
+// 	gpiod_set_value_cansleep(mipi->dc, 1);
+// 	ret = mipi_dbi_spi_transfer(spi, speed_hz, 8, data, num);
+//  free:
+// 	kfree(buf);
+
+// 	return ret;
 }
 
 static void waveshare_enable(struct drm_simple_display_pipe *pipe,
@@ -117,21 +176,22 @@ static void waveshare_enable(struct drm_simple_display_pipe *pipe,
 	mipi_dbi_command(dbi, MIPI_DCS_EXIT_SLEEP_MODE);
 	msleep(250);
 
-	mipi_dbi_command(dbi, MIPI_DCS_SET_PIXEL_FORMAT, 0x55);
+	mipi_dbi_command(dbi, MIPI_DCS_SET_PIXEL_FORMAT, 0x66);
 
 	mipi_dbi_command(dbi, ILI9486_PWCTRL1, 0x44);
 
 	mipi_dbi_command(dbi, ILI9486_VMCTRL1, 0x00, 0x00, 0x00, 0x00);
 
-	mipi_dbi_command(dbi, ILI9486_PGAMCTRL,
-			 0x0F, 0x1F, 0x1C, 0x0C, 0x0F, 0x08, 0x48, 0x98,
-			 0x37, 0x0A, 0x13, 0x04, 0x11, 0x0D, 0x0);
-	mipi_dbi_command(dbi, ILI9486_NGAMCTRL,
-			 0x0F, 0x32, 0x2E, 0x0B, 0x0D, 0x05, 0x47, 0x75,
-			 0x37, 0x06, 0x10, 0x03, 0x24, 0x20, 0x00);
-	mipi_dbi_command(dbi, ILI9486_DGAMCTRL,
-			 0x0F, 0x32, 0x2E, 0x0B, 0x0D, 0x05, 0x47, 0x75,
-			 0x37, 0x06, 0x10, 0x03, 0x24, 0x20, 0x00);
+	// mipi_dbi_command(dbi, ILI9486_PGAMCTRL,
+	// 		 0x0F, 0x1F, 0x1C, 0x0C, 0x0F, 0x08, 0x48, 0x98,
+	// 		 0x37, 0x0A, 0x13, 0x04, 0x11, 0x0D, 0x0);
+	// mipi_dbi_command(dbi, ILI9486_NGAMCTRL,
+	// 		 0x0F, 0x32, 0x2E, 0x0B, 0x0D, 0x05, 0x47, 0x75,
+	// 		 0x37, 0x06, 0x10, 0x03, 0x24, 0x20, 0x00);
+	// mipi_dbi_command(dbi, ILI9486_DGAMCTRL,
+	// 		 0x0F, 0x32, 0x2E, 0x0B, 0x0D, 0x05, 0x47, 0x75,
+	// 		 0x37, 0x06, 0x10, 0x03, 0x24, 0x20, 0x00);
+	mipi_dbi_command(dbi, MIPI_DCS_ENTER_NORMAL_MODE);
 
 	mipi_dbi_command(dbi, MIPI_DCS_SET_DISPLAY_ON);
 	msleep(100);
@@ -160,23 +220,25 @@ static void waveshare_enable(struct drm_simple_display_pipe *pipe,
 }
 
 static const struct drm_simple_display_pipe_funcs waveshare_pipe_funcs = {
-	DRM_MIPI_DBI_SIMPLE_DISPLAY_PIPE_FUNCS(waveshare_enable),
+	.enable = waveshare_enable,
+	.disable = mipi_dbi_pipe_disable,
+	.update = mipi_dbi_pipe_update,
 };
 
 static const struct drm_display_mode waveshare_mode = {
 	DRM_SIMPLE_MODE(480, 320, 73, 49),
 };
 
-DEFINE_DRM_GEM_DMA_FOPS(ili9486_fops);
+DEFINE_DRM_GEM_CMA_FOPS(ili9486_fops);
 
 static const struct drm_driver ili9486_driver = {
 	.driver_features	= DRIVER_GEM | DRIVER_MODESET | DRIVER_ATOMIC,
 	.fops			= &ili9486_fops,
-	DRM_GEM_DMA_DRIVER_OPS_VMAP,
-	DRM_FBDEV_DMA_DRIVER_OPS,
+	DRM_GEM_CMA_DRIVER_OPS_VMAP,
 	.debugfs_init		= mipi_dbi_debugfs_init,
 	.name			= "ili9486",
 	.desc			= "Ilitek ILI9486",
+	.date			= "20200118",
 	.major			= 1,
 	.minor			= 0,
 };
@@ -190,8 +252,6 @@ MODULE_DEVICE_TABLE(of, ili9486_of_match);
 
 static const struct spi_device_id ili9486_id[] = {
 	{ "ili9486", 0 },
-	{ "rpi-lcd-35", 0 },
-	{ "piscreen", 0 },
 	{ }
 };
 MODULE_DEVICE_TABLE(spi, ili9486_id);
@@ -233,7 +293,7 @@ static int ili9486_probe(struct spi_device *spi)
 		return ret;
 
 	dbi->command = waveshare_command;
-	dbi->read_commands = NULL;
+	// dbi->read_commands = NULL;
 
 	ret = mipi_dbi_dev_init(dbidev, &waveshare_pipe_funcs,
 				&waveshare_mode, rotation);
@@ -248,17 +308,19 @@ static int ili9486_probe(struct spi_device *spi)
 
 	spi_set_drvdata(spi, drm);
 
-	drm_client_setup(drm, NULL);
+	drm_fbdev_generic_setup(drm, 0);
 
 	return 0;
 }
 
-static void ili9486_remove(struct spi_device *spi)
+static int ili9486_remove(struct spi_device *spi)
 {
 	struct drm_device *drm = spi_get_drvdata(spi);
 
 	drm_dev_unplug(drm);
 	drm_atomic_helper_shutdown(drm);
+
+	return 0;
 }
 
 static void ili9486_shutdown(struct spi_device *spi)
